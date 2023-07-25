3fd4db059b4dd7defbefa4ff27dc6cdaaf617f41
Vitaly Zimin <infovitz7@gmail.com>
2023-07-25 23:58:21 +0300

    Решил 2 домашних задания

---
+++ Home work seminar 1/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Task 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+Примеры
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+ /*Program
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Max = " + num1);
+}
+else
+{
+    Console.WriteLine("Max = " + num2);
+}
+*/ 
+
+/*Task 4: Напишите программу, которая принимает на вход три числа
+ и выдаёт максимальное из этих чисел.
+Примеры:
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+/*Program
+
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+    max = num2;
+
+if (num3 > max)
+    max = num3;
+        
+Console.WriteLine("max = " + max);
+*/
+
+Task 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+
